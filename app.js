d473a648b8d3dfcbdf2eecd62cf329ef5463bcb6
andyalam <andyalam@hotmail.com>
2017-11-10 08:45:58 -0500

    messing with routing

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require('./lib/env/env').load();
 const Server = require('./lib/server/server');
+const Router = require('./lib/router/router');
 
 const server = new Server();
+const router = new Router();
+router.register('/', function (req, res) {
+	console.log('hit');
+});
+server.use(router);
 
 module.exports = server;
