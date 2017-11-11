8ee93eadfaae42b20fe9871d5633b20b0547c56b
andyalam <andyalam@hotmail.com>
2017-11-11 10:05:18 -0500

    basic routing working

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 require('./lib/env/env').load();
-const Server = require('./lib/server/server');
-const Router = require('./lib/router/router');
+const { Server } = require('./lib');
+const { Router } = require('./lib');
+
+const router = new Router();
+const testRoute = (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'application/json');
+	res.end('{"test": "Success"}');
+};
+router.register('/', testRoute);
+router.register('/health', testRoute);
+console.log(router);
 
 const server = new Server();
-const router = new Router();
-router.register('/', function (req, res) {
-	console.log('hit');
-});
 server.use(router);
 
 module.exports = server;
